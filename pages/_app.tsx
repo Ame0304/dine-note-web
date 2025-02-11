ce508fb52e8b2a196c5a42c9f941981a95f9658b
Ame0304 <rhj903265106@gmail.com>
2025-02-11 12:18:11 -0500

    Define page title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative">
-      <Navbar />
+    <>
+      <Head>
+        <title>DineNote</title>
+        <link rel="icon" href="logo.png" />
+      </Head>
+      <div className="antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative">
+        <Navbar />
 
-      <div className="flex-1 px-8 py-12 grid">
-        <main className="max-w-7xl mx-auto w-full">
-          <Component {...pageProps} />
-        </main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">
+            <Component {...pageProps} />
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
